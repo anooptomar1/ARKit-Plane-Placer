03bf4f4092fede6393fd280911dc8710f5326524
Taylor Guidon <tguidon@gmail.com>
2017-07-21 14:04:38 -0400

    init basic project

---
+++ ARKit-Plane-Placer/ARKit-Plane-Placer/Plane.swift
@@ -0,0 +1,16 @@
+//
+//  Plane.swift
+//  ARKit-Plane-Placer
+//
+//  Created by Taylor Guidon on 7/21/17.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+class Plane: SCNNode {
+    var anchor: ARPlaneAnchor
+    
+}
+
